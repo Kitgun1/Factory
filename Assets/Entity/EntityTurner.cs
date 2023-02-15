a1279d31b7f1a95ff37a40ea6a5f1abef561ba4b
Monkey <azaner228@gmail.com>
2023-02-15 18:41:00 +0300

    feat: Turner

---
+++ Assets/Entity/EntityTurner.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using DG.Tweening;
+
+public static class EntityTurner
+{
+    private const float _rotationStep = 90;
+
+    public static void Action(Transform transform, float rotateDuration)
+    {
+        Vector3 targetRotation = transform.rotation.eulerAngles;
+
+        targetRotation.z += _rotationStep;
+        transform.DORotate(targetRotation, rotateDuration, RotateMode.Fast);
+    }
+}
