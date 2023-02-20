ec0833f7cf637b31b83bc1eecd2902efea733c61
Kimicu <kitgunplay@gmail.com>
2023-02-20 20:52:12 +0300

    Feat: MathK

---
+++ Assets/KiMath/MathK.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace KiMath
+{
+    public static class MathK
+    {
+        public static int RoundFloatToInt(this float value)
+        {
+            if (value >= 0f && Math.Abs(value % 1) < 0.5f)
+                value -= value % 1;
+            else if (value >= 0f)
+                value += 1 - value % 1;
+            else if (value < 0f && Math.Abs(value % 1) > 0.5f)
+                value -= 1 + value % 1;
+            else
+                value -= value % 1;
+
+            return (int)value;
+        }
+
+        public static Vector2Int RoundVector2ToVector2Int(this Vector2 value)
+        {
+            Vector2Int vectorInt = new Vector2Int();
+            vectorInt.x = value.x.RoundFloatToInt();
+            vectorInt.y = value.y.RoundFloatToInt();
+
+            return vectorInt;
+        }
+    }
+}
