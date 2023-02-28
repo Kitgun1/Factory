c4a6c6eb9663db78d2b7118b84a134897423856c
Monkey <azaner228@gmail.com>
2023-02-28 22:06:05 +0300

    feat: money converter

--- Assets/KiMath/MathK.cs
+++ Assets/KiMath/MathK.cs
@@ -1,5 +1,6 @@
 using Factory;
 using System;
+using System.Numerics;
 using UnityEngine;
 
 namespace KiMath
@@ -98,6 +99,67 @@ namespace KiMath
         private static bool NumberInBorders(float number, FloatRange range) => number < range.Max && number > range.Min;
 
         private static bool NumberInBorders(int number, IntRange range) => number < range.Max && number > range.Min;
+
+        #endregion
+
+        #region MoneyCoverter
+
+        private const string Thousand = "K";
+        private const string Million = "M";
+        private const string Billion = "B";
+        private const string Trillion = "T";
+        private const int Indent = 1000;
+
+        public static string ConvertMoney(this BigInteger value)
+        {
+            int tryCount = 0;
+            float newValue = (float)FindMagnitude(value, ref tryCount);
+            string postfix;
+
+            switch (tryCount)
+            {
+                case 0:
+                    postfix = Thousand;
+                    break;
+                case 1:
+                    postfix = Million;
+                    break;
+                case 2:
+                    postfix = Billion;
+                    break;
+                case 3:
+                    postfix = Trillion;
+                    break;
+                default:
+                    postfix = FindLetter(tryCount - 4);
+                    break;
+            }
+
+            return newValue + postfix;
+        }
+
+        private static BigInteger FindMagnitude(BigInteger value, ref int tryCount)
+        {
+            if (value <= Indent)
+            {
+                return value;
+            }
+            else
+            {
+                tryCount++;
+                return FindMagnitude(value / Indent, ref tryCount);
+            }
+        }
+
+        private static string FindLetter(int degree)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (degree <= alphabet.Length)
+                return alphabet[degree].ToString();
+            else
+                return "MANY";
+        }
 
         #endregion
     }
